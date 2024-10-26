c46c53aa70fa608496b6bc84acf25ad25307100c
t0l9 <t0l4ik.kas1@gmail.com>
2024-10-26 21:49:11 +0500

    add new bcc case

--- src/test/java/quruqa/SelenideTest.java
+++ src/test/java/quruqa/SelenideTest.java
@@ -1,8 +1,7 @@
 package quruqa;
 
-import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,9 +12,7 @@ import java.util.stream.Stream;
 
 import static com.codeborne.selenide.CollectionCondition.*;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
 
@@ -35,7 +32,36 @@ public class SelenideTest {
         open("https://selenide.org/");
         $$("#languages a").find(text(locale.name())).click();
         $$(".main-menu-pages a")
-                .filter(visible)
+                //.filter(visible)
                 .shouldHave(texts(buttons));
+    }
+
+
+
+
+
+
+
+    static Stream<Arguments> checkCorrectLanguage(){
+        return Stream.of(
+                Arguments.of("РУС", "Вход в BCC Business"),
+                Arguments.of("QAZ", "BCC Business кіру"),
+                Arguments.of("ENG", "Login BCC Business")
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest(name = "Для локали {0} отображается текст формы {1}")
+    @Tag("LOW")
+    void checkCorrectLanguage(String language, String textMenu){
+
+        Configuration.browser = "FIREFOX";
+
+        open("https://business.bcc.kz/");
+        $(".ant-select-selection-item.ng-star-inserted").click();
+        $(".ant-select-item.ant-select-item-option.ng-star-inserted")
+                .find(String.valueOf(text(language))).click();
+        $(".ant-form").shouldHave(text(textMenu));
+
     }
 }
